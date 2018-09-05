6f26d21dae83a88fbf5c2537410bf65103d1712c
Eric Metcalf <eric@Erics-Air.domain>
2018-09-05 10:38:40 -0700

    Add javadoc on using metric.

--- src/cayuse/exercise/service/WeatherRetriever.java
+++ src/cayuse/exercise/service/WeatherRetriever.java
@@ -2,6 +2,9 @@ package cayuse.exercise.service;
 
 import cayuse.exercise.service.data.WeatherData;
 
+/**
+ * Returns weather data in metric units. 
+ */
 public interface WeatherRetriever {
 	WeatherData getWeatherData(int zipCode);
 }
