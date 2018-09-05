4e4bee9b1c07c9871722399691d712183ed5ff05
Eric Metcalf <eric@Erics-Air.domain>
2018-09-05 08:25:41 -0700

    Forgot to add test class in commit.

--- test/cayuse/exercise/LocationTest.java
+++ test/cayuse/exercise/LocationTest.java
@@ -17,47 +17,47 @@ public class LocationTest {
 		metaData.setElevation(10000);
 
 		assertEquals(
-				"At the location Portland, the temperature is 101, the timezone is Pacific Standard Time, and the elevation is 10,000\n",
+				"At the location Portland, the temperature is 101*C, the timezone is Pacific Standard Time, and the elevation is 10,000 meters\n",
 				Location.getFormattedMetaData(metaData));
 
 		metaData.setTemperature(45);
 		metaData.setElevation(25);
 		assertEquals(
-				"At the location Portland, the temperature is 45, the timezone is Pacific Standard Time, and the elevation is 25\n",
+				"At the location Portland, the temperature is 45*C, the timezone is Pacific Standard Time, and the elevation is 25 meters\n",
 				Location.getFormattedMetaData(metaData));
 
 		metaData.setTemperature(2);
 		assertEquals(
-				"At the location Portland, the temperature is 2, the timezone is Pacific Standard Time, and the elevation is 25\n",
+				"At the location Portland, the temperature is 2*C, the timezone is Pacific Standard Time, and the elevation is 25 meters\n",
 				Location.getFormattedMetaData(metaData));
 
 		metaData.setTemperature(0);
 		metaData.setElevation(0);
 		assertEquals(
-				"At the location Portland, the temperature is 0, the timezone is Pacific Standard Time, and the elevation is 0\n",
+				"At the location Portland, the temperature is 0*C, the timezone is Pacific Standard Time, and the elevation is 0 meters\n",
 				Location.getFormattedMetaData(metaData));
 
 		metaData.setTemperature(-3);
 		metaData.setElevation(-44);
 		assertEquals(
-				"At the location Portland, the temperature is -3, the timezone is Pacific Standard Time, and the elevation is -44\n",
+				"At the location Portland, the temperature is -3*C, the timezone is Pacific Standard Time, and the elevation is -44 meters\n",
 				Location.getFormattedMetaData(metaData));
 
 		metaData.setTemperature(-55);
 		assertEquals(
-				"At the location Portland, the temperature is -55, the timezone is Pacific Standard Time, and the elevation is -44\n",
+				"At the location Portland, the temperature is -55*C, the timezone is Pacific Standard Time, and the elevation is -44 meters\n",
 				Location.getFormattedMetaData(metaData));
 		
 		metaData.setTemperature(8.5);
 		metaData.setElevation(12.6);
 		assertEquals(
-				"At the location Portland, the temperature is 9, the timezone is Pacific Standard Time, and the elevation is 13\n",
+				"At the location Portland, the temperature is 9*C, the timezone is Pacific Standard Time, and the elevation is 13 meters\n",
 				Location.getFormattedMetaData(metaData));
 		
 		metaData.setTemperature(8.4);
 		metaData.setElevation(12.5);
 		assertEquals(
-				"At the location Portland, the temperature is 8, the timezone is Pacific Standard Time, and the elevation is 12\n",
+				"At the location Portland, the temperature is 8*C, the timezone is Pacific Standard Time, and the elevation is 12 meters\n",
 				Location.getFormattedMetaData(metaData));
 
 	}
